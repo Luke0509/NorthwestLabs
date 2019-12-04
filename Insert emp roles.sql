a67a312a0ea36025a1841480b4c5d3f0f9e28f0a
Joshua Thayne <joshthay@byu.edu>
2019-12-03 20:41:28 -0700

    Just some scripts

---
+++ Insert emp roles.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO Employee_Roles (Role_Description, Role_Permissions)
+VALUES ("Sales Agent", "Enter customer information,
+						Contact client,
+						Give price quotes,
+						See new costs to forecast price/revenue")
+
+INSERT INTO Employee_Roles (Role_Description, Role_Permissions)
+VALUES ("Head of Pharmacology/Billing and Receivables", "Review daily compound list from Singapore office, Calculate work order cost amounts and update customer balance, Maintain/update work orders, Verify invoice information before sending invoice")
+
+INSERT INTO Employee_Roles (Role_Description, Role_Permissions)
+VALUES ("Lab Tech", "Perform Lab tests")
+
+INSERT INTO Employee_Roles (Role_Description, Role_Permissions)
+VALUES ("Tech Director of Pharmacology", "View/Edit Results Report, Send results to customer")
+
---
+++ alter query.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE Payment_Information
+ALTER COLUMN Billing_State VARCHAR(2) NULL;
