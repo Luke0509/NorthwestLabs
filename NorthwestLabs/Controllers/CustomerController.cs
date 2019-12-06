9fead7c9c17543ea97465266e36df2c2350599d7
Joshua Thayne <joshthay@byu.edu>
2019-12-06 08:53:37 -0700

    fixed layouts and added Customer work order creation

--- NorthwestLabs/Controllers/CustomerController.cs
+++ NorthwestLabs/Controllers/CustomerController.cs
@@ -38,12 +38,51 @@ namespace NorthwestLabs.Controllers
             }
             return View(lstWO);
         }
-
+        [HttpGet]
         public ActionResult CreateOrder()
         {
-            ViewBag.Message = "New Work Order";
-
+            ViewBag.Customers = db.Customers.ToList();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOrder([Bind(Include = "Order_ID, Cust_ID, Date_Created, Expedite_Order, Test2_IfActive, Test2_IfInactive, Analysis_Completed")] WorkOrders wo)
+        {
+            if (ModelState.IsValid)
+            {
+                //add entry
+                db.WorkOrders.Add(wo);
+                //edit entries
+                //db.Entry(wo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("MyOrders");
+            }
+
+            return View(wo);
+        }
+
+        public ActionResult AddCompound(int? id)
+        {
+            List<WorkOrders> lstorders = new List<WorkOrders>();
+            lstorders.Add(db.WorkOrders.Find(id));
+            ViewBag.Assays = db.Assays.ToList();
+            ViewBag.Orders = lstorders;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCompound([Bind(Include = "LT_Number, Assay_ID, Order_ID, Date_Arrived, Date_Due, Compound_Weight_Client, Compound_Appearance, Molecular_Mass")] Compound_Samples cs)
+        {
+            if (ModelState.IsValid)
+            {
+                //add entry
+                db.Compound_Samples.Add(cs);
+                //edit entries
+                //db.Entry(wo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("MyOrders");
+            }
+
+            return View(cs);
         }
 
         public ActionResult OrderProgress(int? id)
